f5d4e7086435d7f2190974e1868c8c5077ee368f
JMZ <jumingzhen@gmail.com>
2025-09-29 01:10:19 +0800

    writing part of BPE_training before merging stage

--- cs336_basics/Tokenizer.py
+++ cs336_basics/Tokenizer.py
@@ -1,3 +1,102 @@
-def train_bpe():
+import os
+import multiprocessing
+from typing import List, Callable
+from typing import BinaryIO
+import regex as re
+import itertools
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+def process_func(text: str):
+    escaped_tokens = [re.escape(token) for token in special_tokens]
+    delimiter = "|".join(escaped_tokens)
+    temp = re.split(delimiter, text)
+    text = ''.join(temp)
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    return re.finditer(PAT, text)
+
+
+def pretokenize(input_path: str | os.PathLike, special_tokens: list[str],):
+    core_count = multiprocessing.cpu_count()
+    print(f"可用逻辑核心数：{core_count}")
+    chunks = []
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, core_count, b"<|endoftext|>")
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            chunks.append(chunk)
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
+    with multiprocessing.Pool() as pool:
+        # 用map保持处理顺序与输入顺序一致
+        results = pool.map(process_func, chunks)
+    return itertools.chain(*results)
+
+def process_string_in_parallel(l: list[str]) -> list[bytes]:
     
     pass
+def train_bpe(
+    input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str],
+    **kwargs,
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+
+    # initialize vocab
+    vocab: dict[int, bytes] = dict()
+    merges: list[tuple[bytes, bytes]] = []
+    for i in range(256):
+        vocab[i] = chr(i).encode("utf-8") 
+    for i, t in enumerate(special_tokens):
+        vocab[256 + i] = t.encode("utf-8")
+
+
+    # pre-tokenize
+    tokens = pretokenize(input_path, special_tokens)
+
+    # merge
+    
